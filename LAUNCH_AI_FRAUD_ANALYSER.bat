40c15ab21b9466aa38076c13feb6fb99f4998ee9
God Bennett <82050128+g0dEngineer@users.noreply.github.com>
2024-02-29 14:26:59 -0500

    Create LAUNCH_AI_FRAUD_ANALYSER.bat

---
+++ LAUNCH_AI_FRAUD_ANALYSER.bat
@@ -0,0 +1,15 @@
+echo off
+rem Author: God Bennett
+
+
+rem set path to python 3.6.3
+rem %AppData% + \Local\Programs\Python\Python39
+path=C:\Users\bennettgq\AppData\Local\Programs\Python\Python36
+
+rem load ai model for inference\aka answer retrieval
+echo loading artificial intelligence model...
+python UI_CORE.py
+
+
+
+pause
