2efb28b0078cf85100ccc841026e22cea3aee420
God Bennett <82050128+g0dEngineer@users.noreply.github.com>
2024-03-05 19:11:28 -0500

    Update LAUNCH_AI_FRAUD_ANALYSER.bat

--- LAUNCH_AI_FRAUD_ANALYSER.bat
+++ LAUNCH_AI_FRAUD_ANALYSER.bat
@@ -3,8 +3,8 @@ rem Author: God Bennett
 
 
 rem set path to python 3.6.3
-rem %AppData% + \Local\Programs\Python\Python39
-path=C:\Users\bennettgq\AppData\Local\Programs\Python\Python36
+rem %AppData% + \Local\Programs\Python\Python36
+path=C:\Users\godbennett\AppData\Local\Programs\Python\Python36
 
 rem load ai model for inference\aka answer retrieval
 echo loading artificial intelligence model...
